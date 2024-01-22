ad7685937b16047e84bed9d33384438729ce48dc
Anish Ansari <147129841+anishans7@users.noreply.github.com>
2024-01-22 10:00:45 +0530

    Add files via upload

---
+++ Digital Clock/apps.js
@@ -0,0 +1,24 @@
+function myclock(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session ="AM"
+    if (h==0){
+        h = 12;
+    }
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+
+    }
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s
+    let time = h + ":" + m + ":" + s + " "+session;
+    document.getElementById("my-Clock").innerText = time;
+    document.getElementById("my-Clock").innerContent =time;
+    setTimeout(myclock,1000);
+
+}
+myclock();
